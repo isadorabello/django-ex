51968173f6f14980a4ea1c9e918c3142b106ee5b
isadorabello <isadora.bello@gec.inatel.br>
2023-02-18 13:11:59 -0300

    feat: adding db

--- galeria/views.py
+++ galeria/views.py
@@ -3,11 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
 def index(request):
+    dados = {
+        1: {"nome": "Um nome aleatorio", "legenda": "fonte.vozes.da.minha.cabeça.org"},
+        2: {"nome": "Um nome pouco comum", "legenda": "fonte.instituto.CDMR.org"}
+    }
     # função que responde a requisição HTTP da página WEB
     # HttpResponse('<h1>Web Test</h1>')
-    return render(request, 'galeria/index.html')
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 
 def imagem(request):
