53659bdb04cbd8231f320a66cd12d8e23fc93fc6
isadorabello <isadora.bello@gec.inatel.br>
2023-02-18 17:33:01 -0300

    feat: adjusting db

--- galeria/views.py
+++ galeria/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
+from galeria.models import Fotografia
 # from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    dados = {
-        1: {"nome": "Um nome aleatorio", "legenda": "fonte.vozes.da.minha.cabeça.org"},
-        2: {"nome": "Um nome pouco comum", "legenda": "fonte.instituto.CDMR.org"}
-    }
+    fotografias = Fotografia.objects.all()
     # função que responde a requisição HTTP da página WEB
     # HttpResponse('<h1>Web Test</h1>')
-    return render(request, 'galeria/index.html', {"cards": dados})
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 
 def imagem(request):
