c3b8ff87ee01b2394a7fb61235395dcec9c75be0
isadorabello <isadora.bello@gec.inatel.br>
2023-02-19 18:04:13 -0300

    feat: admin server

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
 # Register your models here.
+
+class Listando(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+
+# Registrou o bando de dados no Admin
+admin.site.register(Fotografia, Listando)
