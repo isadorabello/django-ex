6785f34a2866d84a031f10194450cdb64abaae47
isadorabello <isadora.bello@gec.inatel.br>
2023-02-20 17:21:05 -0300

    feat: adjusting DB

--- galeria/admin.py
+++ galeria/admin.py
@@ -4,9 +4,11 @@ from galeria.models import Fotografia
 # Register your models here.
 
 class Listando(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "categoria")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_per_page = 10
 
 # Registrou o bando de dados no Admin
 admin.site.register(Fotografia, Listando)
--- galeria/models.py
+++ galeria/models.py
@@ -3,10 +3,23 @@ from django.db import models
 # Create your models here.
 
 class Fotografia(models.Model):
+    
+    # AS OPÇÕES DEVEM SER EM FORMATO DE TUPLA, POIS É A MANEIRA QUE O METODO CHARFIELD ENTENDE
+    OPCOES_CATEGORIAS = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA", "Estrela"),
+        ("GALÁXIA", "Galáxia"),
+        ("PLANETA", "Planeta")
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    # TODA VEZ QUE O BANCO DE DADOS É ALTERADO (QUE O ARQUIVO MODEL.PY É ALTERADO), É NECESSÁRIO FAZER UMA MIGRATION NOVAMENTE
+    # python manage.py makemigrations -> python manage.py migrate
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIAS,default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
 
 
     def __str__(self) -> str:
