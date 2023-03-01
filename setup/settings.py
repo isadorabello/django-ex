a827ee08f92e0fad755886cff566c96a135dbc56
isadorabello <isadora.bello@gec.inatel.br>
2023-03-01 00:14:40 -0300

    feat: adjusting messages matters

--- setup/settings.py
+++ setup/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path, os
 from dotenv import load_dotenv
 
@@ -141,3 +142,10 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# MESSAGES
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
